34e49e09b184a11ff78d6fc5121d7761f0a02e26
clipcrisp <>
2025-01-04 14:26:22 -0500

    Add .out to executable so the ignore catches them.

--- g1-raygame/build.fish
+++ g1-raygame/build.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
-cc $argv[1..-1] -MJ main.o.json -L/opt/homebrew/lib/ -I/opt/homebrew/include/ -std=c23 -Wall -lm -lraylib -o test 
+cc $argv[1..-1] -MJ main.o.json -L/opt/homebrew/lib/ -I/opt/homebrew/include/ -std=c23 -Wall -lm -lraylib -o test.out 
 sed -e '1s/^/[\n/' -e '$s/,$/\n]/' *.o.json > compile_commands.json
 rm *.o.json
